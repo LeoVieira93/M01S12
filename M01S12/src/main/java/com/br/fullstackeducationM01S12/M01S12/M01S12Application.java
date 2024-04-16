6270f9df93351484876ff778668677da443e3f2b
Leonardo Vieira <leonardovieira1993@gmail.com>
2024-04-16 01:06:31 -0300

    primeiro commit

---
+++ M01S12/src/main/java/com/br/fullstackeducationM01S12/M01S12/M01S12Application.java
@@ -0,0 +1,13 @@
+package com.br.fullstackeducationM01S12.M01S12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M01S12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M01S12Application.class, args);
+	}
+
+}
---
+++ M01S12/src/test/java/com/br/fullstackeducationM01S12/M01S12/M01S12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fullstackeducationM01S12.M01S12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M01S12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
